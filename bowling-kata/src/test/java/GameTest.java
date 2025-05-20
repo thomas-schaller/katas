45e994ceccfd4fd95fc01da9764278ef3e81f5ff
Thomas Schaller <th.schaller@gmail.com>
2025-05-20 23:00:27 +0200

    first compiled version.

--- bowling-kata/src/test/java/GameTest.java
+++ bowling-kata/src/test/java/GameTest.java
@@ -1,7 +1,15 @@
-
-
-
+import bowling.Game;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class GameTest {
 
+   @Test
+   public void scoreOneRollWithoutBonus()
+   {
+      Game g = new Game();
+      g.roll(3);
+      g.roll(4);
+      Assert.assertEquals(3+4,g.score());
+   }
    }
---
+++ bowling-kata/src/test/java/bowling/Game.java
@@ -0,0 +1,10 @@
+package bowling;
+
+public class Game {
+    public void roll(int i) {
+    }
+
+    public int score() {
+        return 7;
+    }
+}
